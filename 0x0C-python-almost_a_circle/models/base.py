932d72dbdcb59ff4a60daa1de2418905ffe6f5c3
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-01-13 11:13:28 +0000

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Base:
+    """
+    Base class for managing id attribute in future classes.
+    """
+
+    __nb_objects = 0  # private class attribute
+
+    def __init__(self, id=None):
+        """
+        Class constructor for Base.
+
+        Args:
+            id (int, optional): If provided,
+            assign it to the public instance attribute 'id'.
+            Otherwise, increment __nb_objects and assign the new value to 'id'.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
