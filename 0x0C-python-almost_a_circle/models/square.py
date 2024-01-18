d65d2d13b1f0c03e7cb47009569e0398aad5bc93
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-01-18 11:03:19 +0000

    Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-"""Defines the Square class"""
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
     """
-    Square class that inherits from Rectangle.
+    Square class that inherits from the Rectangle class.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -16,59 +13,27 @@ class Square(Rectangle):
         Class constructor for Square.
 
         Args:
-            size (int): Size of the square.
+            size (int): Size of the square (width and height).
             x (int, optional): X-coordinate of the square's position.
             y (int, optional): Y-coordinate of the square's position.
-            id (int, optional): Identifier for the square. If None,
-            the base class logic is used.
-
-        Note:
-            The super() call is used to invoke
-            the constructor of the Rectangle class.
+            id (int, optional): Identifier for the square.
+            If None, the base class logic is used.
         """
         super().__init__(size, size, x, y, id)
 
-    def to_dictionary(self):
-        """
-        Return the dictionary representation of the Square.
+    @property
+    def size(self):
+        """Getter for the size attribute."""
+        return self.width  # Since size is the same as width
 
-        Returns:
-            dict: Dictionary containing attributes of the Square instance.
-        """
-        return {
-            'id': self.id,
-            'size': self.width,  # Size attribute is represented by the width
-            'x': self.x,
-            'y': self.y
-        }
-
-    def update(self, *args, **kwargs):
-        """
-        Update attributes of the Square instance.
-
-        Args:
-            *args: List of arguments (id, size, x, y) - no-keyworded arguments.
-            **kwargs: Dictionary of keyworded arguments.
-
-        Note:
-            **kwargs must be skipped if *args exists and is not empty.
-        """
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for i, arg in enumerate(args[:len(attrs)]):
-                setattr(self, attrs[i], arg)
-
-        if kwargs and not args:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+    @size.setter
+    def size(self, value):
+        """Setter for the size attribute."""
+        self.width = value
+        self.height = value
 
     def __str__(self):
-        """
-        Override the __str__ method to return the string representation.
-
-        Returns:
-            str: Formatted string representation of the Square instance.
-        """
+        """Override the __str__ method to return the string representation."""
         return (
             f"[Square] ({self.id}) {self.x}/{self.y} - "
             f"{self.width}"
