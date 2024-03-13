4cf1943e97ce8258c34ec94f2f333fe48f9bf33a
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-03-13 01:06:09 +0000

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function convertToBase (num) {
-    const alphabet = 'ABCDEF';
-    if (num === 0) {
-      return '';
-    } else {
-      let remainder = num % base;
-      if (base === 16 && remainder >= 10) {
-        remainder = alphabet[remainder - 10];
-      }
-      return convertToBase(Math.floor(num / base)) + remainder;
-    }
-  };
+  function converted (number) {
+    return number.toString(base);
+  }
+  return converted;
 };
