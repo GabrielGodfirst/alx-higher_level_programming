5a073c607fc378fe5cdae5983818beb6b473ee0f
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-03-13 00:56:23 +0000

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,10 +2,15 @@
 
 exports.converter = function (base) {
   return function convertToBase (num) {
+    const alphabet = 'ABCDEF';
     if (num === 0) {
       return '';
     } else {
-      return convertToBase(Math.floor(num / base)) + (num % base);
+      let remainder = num % base;
+      if (base === 16 && remainder >= 10) {
+        remainder = alphabet[remainder - 10];
+      }
+      return convertToBase(Math.floor(num / base)) + remainder;
     }
   };
 };
