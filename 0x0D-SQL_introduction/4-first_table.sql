21934aa385d9775ff1a0cf580c31d4ba6aedfccb
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-03-15 09:48:13 +0000

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Create a table called first_table if it does not already exist.
+
+-- The table has two columns: id of type INT and name of type VARCHAR(256).
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+
