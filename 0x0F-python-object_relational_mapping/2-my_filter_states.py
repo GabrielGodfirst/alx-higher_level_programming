a88ad468bcf0594f670d350ad0340f411648b832
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-03-18 10:45:22 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
 """
-Write a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
 import MySQLdb
 from sys import argv
 
-# Code executes when imported
+# The code should not be executed when imported
 if __name__ == '__main__':
 
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # Code enables multiple seperate working environments
-    # from same connection to the database.
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
