a8d02599b2ab93fc3917dca439b2046ce69b1f89
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-04-24 00:49:49 +0000

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,9 +6,5 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`Content written to ${filePath} successfully.`);
+  if (err) console.log(err);
 });
