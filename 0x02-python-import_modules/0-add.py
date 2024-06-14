13538519488e6fe5e5425212e3fb84bf58948f20
GabrielGodfirst <43904195+GabrielGodfirst@users.noreply.github.com>
2024-06-14 10:42:54 +0000

    Readme and add codes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     from add_0 import add
 
     a = 1
     b = 2
 
-    # Import the add function from add_0.py
-    exec(open("add_0.py").read())
-
-    # Use the add function and print the result using string formatting
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
